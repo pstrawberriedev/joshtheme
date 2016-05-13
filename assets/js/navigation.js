0bd178b37b68500d88e896d6024afc5359f2b2e6
unknown <josh.depaolo@01DePaoloJ.wsmcorp.com>
2016-05-13 14:19:18 -0600

    started mobile nav js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,14 +4,36 @@
 
 console.log('--> navigation.js');
 
+// Vars
 var hamburger = $('#mobile-menu');
+var mobileNav = $('#mobile-nav');
 
 // Window Scroll functions
 $(window).on('scroll', function() {
     scrollPosition = $(this).scrollTop();
+    
+    // Hamburger Opacity
     if (scrollPosition >= 70) {
       TweenLite.to(hamburger, 0.2, { autoAlpha: .2, ease: Power1.easeInOut });
     } else {
       TweenLite.to(hamburger, 0.2, { autoAlpha: 1, ease: Power1.easeInOut });
     }
+  
+});
+
+// Nav Menu
+hamburger.on('click', function() {
+
+  
+  
+  // Animation
+  var tl = new TimelineLite({onComplete:completed});
+  tl.to(mobileNav, .5, { x: 0, ease: Power1.easeOut })
+  
+  
+  // Callback
+  function completed() {
+    console.log('completed');
+  }
+  
 });
