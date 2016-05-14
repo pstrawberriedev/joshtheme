ba6e2b375969ca41b834cb54e9b12d0aa8e72274
JD <pstrawberrie@gmail.com>
2016-05-13 18:11:22 -0600

    nav animations finished. classic close, outside click close, esc key close, drag close

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -7,6 +7,8 @@ console.log('--> navigation.js');
 // Vars
 var hamburger = $('#mobile-menu');
 var mobileNav = $('#mobile-nav');
+var mobileNavBounds = $('#mobile-nav-bounds');
+var mobileNavOverlap = $('#mobile-nav-overlap');
 
 // Window Scroll functions
 $(window).on('scroll', function() {
@@ -15,25 +17,60 @@ $(window).on('scroll', function() {
     // Hamburger Opacity
     if (scrollPosition >= 70) {
       TweenLite.to(hamburger, 0.2, { autoAlpha: .2, ease: Power1.easeInOut });
+      hamburger.addClass('scrolled');
     } else {
       TweenLite.to(hamburger, 0.2, { autoAlpha: 1, ease: Power1.easeInOut });
+      hamburger.removeClass('scrolled');
     }
   
 });
 
 // Nav Menu
+function closeNav() {
+  TweenLite.to(mobileNav, .42, { x: "-500px", autoAlpha:0, ease: Power1.easeOut })
+  TweenLite.to(hamburger, .3, { color:"#424242", ease: Power1.easeOut })
+  hamburger.removeClass('active');
+  mobileNav.attr("aria-hidden","true");
+}
+function openNav() {
+  TweenLite.to(mobileNav, .3, { x: 0, autoAlpha:1, ease: Power1.easeOut })
+  TweenLite.to(hamburger, .3, { color:"#fff", ease: Power1.easeOut })
+  hamburger.addClass('active');
+  mobileNav.attr("aria-hidden","false");
+}
 hamburger.on('click', function() {
-
   
-  
-  // Animation
-  var tl = new TimelineLite({onComplete:completed});
-  tl.to(mobileNav, .5, { x: 0, ease: Power1.easeOut })
-  
-  
-  // Callback
-  function completed() {
-    console.log('completed');
+  // Default Action
+  if(! hamburger.hasClass('active')) {
+    openNav();
+  } else {
+    closeNav();
   }
   
+  // Close on Document Click
+  $(document).on('click', function(event) {
+    if(!$(event.target).closest(hamburger).length && !$(event.target).closest(mobileNav).length && hamburger.hasClass('active')) {
+      closeNav();
+    }
+  });
+
+  // Close with Escape key
+  $(document).keyup(function(e) {
+    if (e.which === 27 && hamburger.hasClass('active')) {
+      closeNav();
+    }
+  });
+  
+});
+
+// Drag Nav to Close
+Draggable.create(mobileNav, {
+  type:"x",
+  bounds: mobileNavBounds,
+  throwProps:true,
+    onDragEnd:function() {
+      if (this.hitTest(mobileNavOverlap, 60)) {
+        closeNav();
+      }
+    }
 });
