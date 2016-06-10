87168a976b3e5637ff31df5b226ca8c1a9bb473a
pstrawberriedev <pstrawberriedev@gmail.com>
2016-06-10 11:45:53 -0600

    updated ark stuff

--- assets/js/ark.js
+++ assets/js/ark.js
@@ -1,61 +1,63 @@
 
 $(document).ready(function() {
   
-  if(!$('.page-ark').length) { return; }
+  if($('.ark-widget').length) {  
   
-  // Set up the Ark Server Status Widget
-  var widget = $('.ark-widget');
-  var widgetStatus = $('.ark-widget .status');
-  var widgetName = $('.ark-widget .name');
-  var widgetPlayers = $('.ark-widget .players');
-  var widgetMap = $('.ark-widget .map');
-  var widgetUpdated = $('.ark-widget .check');
-  
-  var widgetConnect = $('.ark-widget .connect');
-  
-  
-  // Get the Ark Server Status json
-  $.ajax({
-    url: 'http://ark.josh.care/status',
-    type: 'GET',
-    dataType: 'json',
+    // Set up the Ark Server Status Widget
+    var widget = $('.ark-widget');
+    var widgetStatus = $('.ark-widget .status');
+    var widgetName = $('.ark-widget .name');
+    var widgetPlayers = $('.ark-widget .players');
+    var widgetMap = $('.ark-widget .map');
+    var widgetUpdated = $('.ark-widget .check');
 
-    success: function (json) {
-      
-      var info = json.josh;
-      var status = json.updated;
+    var widgetConnect = $('.ark-widget .connect');
 
-      var serverStatus = info.status;
-      var lastCheckedDate = status.date;
-      var lastCheckedMinutes = status.minutes;
 
-      //console.log(info);
-      
-      // Populate Widget Info
-      widgetStatus.addClass(serverStatus);
-      widgetConnect.addClass(serverStatus);
-      widgetName.text(info.serverName);
-      
-      if(serverStatus === "up") {
-        widgetPlayers.text(info.numberOfPlayers + '/' + info.maxNumberOfPlayers);
-        widgetMap.text(' playing on ' + info.map);
-        widgetUpdated.text('Status updated at ' + lastCheckedMinutes);
-        widgetConnect.attr('href', info.connectUrl);
-      }
-      else {
+    // Get the Ark Server Status json
+    $.ajax({
+      url: 'http://ark.josh.care/status',
+      type: 'GET',
+      dataType: 'json',
+
+      success: function (json) {
+
+        var info = json.josh;
+        var status = json.updated;
+
+        var serverStatus = info.status;
+        var lastCheckedDate = status.date;
+        var lastCheckedMinutes = status.minutes;
+
+        //console.log(info);
+
+        // Populate Widget Info
+        widgetStatus.addClass(serverStatus);
+        widgetConnect.addClass(serverStatus);
+        widgetName.text(info.serverName);
+
+        if(serverStatus === "up") {
+          widgetPlayers.text(info.numberOfPlayers + '/' + info.maxNumberOfPlayers);
+          widgetMap.text(' playing on ' + info.map);
+          widgetUpdated.text('Status updated at ' + lastCheckedMinutes);
+          widgetConnect.attr('href', info.connectUrl);
+        }
+        else {
+          widgetPlayers.text("Server is down");
+        }
+
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+
         widgetPlayers.text("Server is down");
+        widgetStatus.addClass('down');
+        widgetConnect.addClass('down');
+        console.log('there was an error getting ark info for ' + url + '\n' + errorThrown);
+
+
       }
-
-    },
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-      
-      widgetPlayers.text("Server is down");
-      widgetStatus.addClass('down');
-      widgetConnect.addClass('down');
-      console.log('there was an error getting ark info for ' + url + '\n' + errorThrown);
-      
-
-    }
-  });
+    });
+    
+  }
   
 });
