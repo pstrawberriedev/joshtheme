e64ffaf95d49ce722dce023804aae88dd5614d77
Josh DePaolo <pstrawberriedev@gmail.com>
2016-09-08 07:19:38 -0600

    nav rework, no more slidey...too many foo paz

--- assets/js/navigation-plus.js
+++ assets/js/navigation-plus.js
@@ -4,13 +4,9 @@
 console.log('--> navigation.js');
 
 (function() {
-  var $pageOverlay = $('#page-overlay');
-  var bodyOverlayClass = 'overlay-active';
 
   var $hamburger = $('#mobile-menu');
   var $mobileNav = $('nav.nav-plus');
-  var $pageContainer = $('#main-content');
-  var currentScroll;
 	
   // Document Ready
   $(document).ready(function() {
@@ -45,33 +41,6 @@ console.log('--> navigation.js');
       });
   });
 
-  // Control Page Overlays
-  // - overlay not necessary with nav-plus, so just applying the body class
-  //
-  function showPageOverlay() {
-    
-      $('body').addClass(bodyOverlayClass);
-
-  }
-
-  function hidePageOverlay() {
-    
-    if(!$('#video').length) { //fullscreen video page
-      setTimeout(function() {
-        $('body').removeClass(bodyOverlayClass);
-        
-        $pageContainer.css('top', 0);
-        window.scrollTo(0,currentScroll);
-        
-        setTimeout(function() {
-          currentScroll = 0;
-        },50)
-        
-      },400)
-    }
-    
-  }
-
   // Hamburger to X
   //
   function doTheHammy(option) {
@@ -102,9 +71,7 @@ console.log('--> navigation.js');
   function closeNav() {
     
     doTheHammy('close');
-    hidePageOverlay();
     TweenLite.to($mobileNav, .3, { width: 0, ease: Power1.easeOut })
-    TweenLite.to($pageContainer, .3, { x: 0, ease: Power1.easeOut})
     $hamburger.removeClass('active');
 
   }
@@ -112,9 +79,7 @@ console.log('--> navigation.js');
   function openNav() {
 
     doTheHammy('open');
-    showPageOverlay();
     TweenLite.to($mobileNav, .3, { width: 305, ease: Power1.easeOut })
-    TweenLite.to($pageContainer, .3, { x: 305, ease: Power1.easeOut })
     $hamburger.addClass('active');
 
   }
@@ -123,11 +88,6 @@ console.log('--> navigation.js');
   //
   $hamburger.on('click', function(e) {
     e.preventDefault();
-    
-    if($('body').scrollTop() != 0) {
-      currentScroll = $('body').scrollTop();
-		  $pageContainer.css('top', -currentScroll);
-		}
     
     // Default Action
     if(! $hamburger.hasClass('active')) {
